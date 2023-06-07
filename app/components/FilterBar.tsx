740733a8dc93811a6eb2d4b2a3e3aab085f5164d
AlexRG <aleksrdev@gmail.com>
2023-06-07 09:51:52 +0300

    FilterBar update

--- app/components/FilterBar.tsx
+++ app/components/FilterBar.tsx
@@ -3,33 +3,25 @@ import React from "react";
 export default function FilterBar() {
   return (
     <nav className="h-8 w-full sm:justify-center items-center flex gap-4 overflow-x-scroll scrollbar-hide px-4 my-4">
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        All
-      </button>
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        New
-      </button>
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Music
-      </button>{" "}
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Blog
-      </button>{" "}
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Live
-      </button>{" "}
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Mixes
-      </button>{" "}
-      <button className="w-25 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Sports
-      </button>{" "}
-      <button className="w-20 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Art
-      </button>{" "}
-      <button className="w-24 h-6 px-4 border border-fuchsia-950 rounded-xl">
-        Travel
-      </button>
+      <Button type={"All"} />
+      <Button type={"New"} />
+      <Button type={"Music"} />
+      <Button type={"Blog"} />
+      <Button type={"Live"} />
+      <Button type={"Mixes"} />
+      <Button type={"Sports"} />
+      <Button type={"Art"} />
+      <Button type={"Travel"} />
     </nav>
+  );
+}
+function Button({ type }: { type: string }) {
+  return (
+    <button
+      className="w-24 h-6 px-4 border border-fuchsia-950 rounded-xl"
+      // onClick={() => function()}
+    >
+      {type}
+    </button>
   );
 }
